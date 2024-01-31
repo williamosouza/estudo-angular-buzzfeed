ade103a8cc187ca48375e6a5e016900ff6636bb4
williamosouza <williamosouza89@hotmail.com>
2024-01-31 00:28:08 -0300

    buzzfeed clone1

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,81 @@
 import { Component } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
+
+
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
+
+
 export class QuizzComponent {
+
+  title:string = ""
+
+  questions:any
+  questionSelected:any
+
+answers:string[] = []
+answersSelected:string = ""
+
+questionIndex:number = 0
+questionMaxIndex:number = 0
+
+finished:boolean = false
+
+  constructor(){
+
+  }
+
+  ngOnInit():void {
+    if(quizz_questions){
+      this.finished = false;
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playerChoose(value:string){
+    this.nextStep()
+    this.answers.push(value)
+  }
+
+  async nextStep(){
+    this.questionIndex+=1
+
+      if(this.questionMaxIndex > this.questionIndex){
+        this.questionSelected = this.questions[this.questionIndex]
+      } else {
+        const finalAnswer:string = await this.checkResults(this.answers)
+        this.finished = true
+        this.answersSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
+      }
+  }
+
+  async checkResults(answers:string[]){
+    const result = answers.reduce((previous, current, i, arr)=>{
+      if ( 
+        arr.filter(item => item === previous).length > 
+        arr.filter(item => item === current).length     
+        ){
+          return previous
+        } 
+        else {
+          return current
+        }
+        
+      })
+
+      return result
+  }
+
+
 
 }
