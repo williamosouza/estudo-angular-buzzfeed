cd6f6003cfae34d1c112cef721ebafe67cf60f29
williamosouza <williamosouza89@hotmail.com>
2024-02-02 15:16:32 -0300

    update draw result

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import quizz_questions from '../../../assets/data/quizz_questions.json';
-
+import draw_questions from '../../../assets/data/draw_questions.json';
 
 
 @Component({
@@ -16,6 +16,8 @@ export class QuizzComponent {
 
   questions:any
   questionSelected:any
+  drawQuestion:any
+  drawQuestionSelected:any
 
 answers:string[] = []
 answersSelected:string = ""
@@ -23,8 +25,14 @@ imageAnswer:string = ""
 
 questionIndex:number = 0
 questionMaxIndex:number = 0
+drawIndex:number = 0
+drawOption:string[] = []
 
 finished:boolean = false
+draw:boolean = false
+buttonCurrent:boolean = false
+buttonDraw:boolean = false
+option: any;
 
   constructor(){
 
@@ -33,15 +41,25 @@ finished:boolean = false
   ngOnInit():void {
     if(quizz_questions){
       this.finished = false;
+      this.draw = false;
+      this.buttonCurrent = false;
+      this.buttonDraw = false;
       this.title = quizz_questions.title
 
       this.questions = quizz_questions.questions
       this.questionSelected = this.questions[this.questionIndex]
+      this.drawQuestion = draw_questions.questions[this.drawIndex]
+      
+
 
       this.questionIndex = 0
       this.questionMaxIndex = this.questions.length
+      this.drawIndex = 0
+      this.drawOption = []
     }
   }
+
+  // ao clicar na resposta, aciona a função que inclui valor no array de resposta e segue o nextstep
 
   playerChoose(value:string){
     this.nextStep()
@@ -49,36 +67,57 @@ finished:boolean = false
     console.log(this.answers)
   }
 
+  // ao clicar, seleciona o resultado final em uma condição de empate
+
+    drawChoose(value:string){
+    console.log(value)
+    this.finished = true
+    this.draw = false
+    this.buttonDraw = false
+    this.answersSelected = quizz_questions.results[value as unknown as keyof typeof quizz_questions.results]
+    this.imageAnswer = quizz_questions.images[value as unknown as keyof typeof quizz_questions.images]
+  }
+
+  // findOptionsDraw devolve somente as opções de texto do arrey gerado nos resultados empatados, dando a opção de somente
+  // escolher entre dois ou mais empatados.
+
+  async findOptionsDraw(id:any){
+    const option:any = this.drawQuestion.options.find((option: { id: any; }) => option.id === id);
+    return option ? option.name : null;
+    }
+
+
+  
+
   async nextStep(){
     this.questionIndex+=1
 
       if(this.questionMaxIndex > this.questionIndex){
         this.questionSelected = this.questions[this.questionIndex]
       } else {
-        const finalAnswer:string = await this.checkResults(this.answers)
-        this.finished = true
-        this.answersSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
-        this.imageAnswer = quizz_questions.images[finalAnswer as keyof typeof quizz_questions.images]
-        console.log(finalAnswer)
+        const finalAnswer:string[] = await this.checkResults(this.answers)
+          if(finalAnswer.length <= 1){
+            this.finished = true
+            this.buttonCurrent = true
+            this.answersSelected = quizz_questions.results[finalAnswer as unknown as keyof typeof quizz_questions.results]
+            this.imageAnswer = quizz_questions.images[finalAnswer as unknown as keyof typeof quizz_questions.images]
+            console.log(finalAnswer)
+          } else {
+            this.drawOption = finalAnswer
+            this.draw = true
+            this.buttonCurrent = true
+            this.buttonDraw = true
+            
+          }
+       
       }
   }
 
-  //async checkResults(answers:string[]){
-  //  const result = answers.reduce((previous, current, i, arr)=>{
-  //    if ( 
-  //      arr.filter(item => item === previous).length > 
-  //      arr.filter(item => item === current).length     
-  //      ){
-  //        return previous
-  //      } 
-  //      else {
-  //        return current
-  //      }
-  //      
-  //    })
-  //   
-  //    return result
-  //}
+  // a função pega o array de respostas e conta as incidências de cada alias. O maior valor de incidências, é reconhecido,
+  // transformando em objeto com o alias e valor. O maior valor é reconhecido. Um novo array é criado somente com os alias
+  // reconhecidos com o maiorValor.
+  // Se houver empate, sai um array com mais de um elemento, que será tratado na lógica de empate. O array de empatados sempre
+  // terá a quantidade máxima de finais possíveis, nada mais.
 
   async checkResults(letras:string[]){
 
@@ -100,9 +139,8 @@ finished:boolean = false
       
     });
     
-    let result:string | null = array.filter(item => item !== null).join("");
+    let result:any[]= array.filter(item => item !== null);
 
-    
     return result    
     
     }
